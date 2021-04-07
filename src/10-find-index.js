42a155aa45bdc4bd1f644b56e5b3e0abda8e75f6
Denis Savitskiy <denissavitski@strim-tech.com>
2021-04-07 20:43:01 +0300

    feat: implement findIndex function

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let i = 0;
+  let len = array.length - 1;
+  let j;
+  while (i <= len) {
+    j = Math.floor((i + len) / 2);
+    if (array[j] < value) {
+      i = j + 1;
+    } else if (array[j] > value) {
+      len = j - 1;
+    } else {
+      return j;
+    }
+  }
+  return -1;
 }
 
 module.exports = findIndex;
