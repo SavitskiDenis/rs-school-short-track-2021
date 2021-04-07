5e6873de344ae3a906a1f7612b28a07b13fa1e06
Denis Savitskiy <denissavitski@strim-tech.com>
2021-04-07 20:41:09 +0300

    feat: implement minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,27 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function getMinesCountAroundByPosition (matrix, i, j) {
+  let count = 0;
+  for (let a = -1; a < 2; a++) {
+    for (let b = -1; b < 2; b++) {
+      if ((i !== i + a || j !== j + b) && matrix[i + a] && matrix[i + a][j + b]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function minesweeper(matrix) {
+  const result = [];
+  matrix.forEach((arr, i) => {
+    result.push([]);
+    arr.forEach((_, j) => {
+      result[i][j] = getMinesCountAroundByPosition(matrix, i, j);
+    });
+  });
+  return result;
 }
 
 module.exports = minesweeper;
