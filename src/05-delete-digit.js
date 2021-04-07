2c5a10a1943f0e512f69adbda5e90d9e18eeac49
Denis Savitskiy <denissavitski@strim-tech.com>
2021-04-07 20:41:25 +0300

    feat: implement deleteDigit function

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,13 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let max = -1;
+  const str = n.toString();
+  for (let i = 0; i < str.length; i++) {
+    max = Math.max(max, Number.parseInt(str.substr(0, i) + str.substr(i + 1), 10));
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
